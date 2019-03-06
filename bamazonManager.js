48729cebb9e81904a38b609ed676632e71a6510e
kanikas01 <larvakurt@yahoo.com>
2019-03-05 17:09:47 -0800

    Add console.table import, add inquirer list, add main and two sub functions

--- bamazonManager.js
+++ bamazonManager.js
@@ -2,12 +2,26 @@
 // ---------- Imports and global vars ---------- //
 
 require('dotenv').config();
+const cTable = require('console.table');
 var customer = require('./db_utils.js');
 var inquirer = require('inquirer');
 
 var connection = customer.connection;
-var generalQuery = customer.generalQuery;
-var queryProducts = customer.queryProducts;
+var showCustomerView = customer.showCustomerView;
+
+var questions = [
+  {
+    type: 'list',
+    name: 'choice',
+    message: 'What would you like to do?',
+    choices: ['View products for sale', 
+              'View low inventory', 
+              'Add to inventory',
+              'Add new product',
+              'Show customer view',
+              'Exit']
+  }
+];
 
 // Connect to DB
 connection.connect(function (err) {
@@ -15,8 +29,73 @@ connection.connect(function (err) {
     console.error('error connecting: ' + err.stack);
     return;
   }
-  // Query products db, passing makePurchase as callback
-  queryProducts(connection, generalQuery);
-  connection.end();
+
+  // Execute main function
+  main();
+
 });
+
+// ---------- Function definitions ---------- //
+
+function main() {
+  inquirer.prompt(questions).then(answers => {
+    switch (answers.choice) {
+      case 'View products for sale':
+        viewProductsForSale(main);
+        break;
+      case 'View low inventory':
+        viewLowInventory(main);
+        break;
+      case 'Add to inventory':
+        break;
+      case 'Add new product':
+        break;
+      case 'Show customer view':
+        showCustomerView(connection, main);
+        break;
+      case 'Exit':
+        connection.end();
+    }
+  });
+}
+
+function viewProductsForSale(callback) {
+  connection.query(`SELECT id,
+                      product_name, 
+                      department_name, 
+                      price,
+                      stock_quantity
+                    FROM products`, function (error, results, fields) {
+    if (error) throw error;
+    console.log();
+    console.table(results);
+    if (callback) {
+      callback();
+    }
+  });
+}
+
+function viewLowInventory(callback) {
+  connection.query(`SELECT id,
+                      product_name AS product, 
+                      department_name AS department, 
+                      price,
+                      stock_quantity AS quantity
+                    FROM products
+                    WHERE stock_quantity < 5`, function (error, results, fields) {
+    if (error) throw error;
+    console.log();
+    if (results.length === 0) {
+      console.log("Inventory is sufficiently stocked.\n");
+    }
+    else {
+      console.table(results);
+    }
+
+    if (callback) {
+      callback();
+    }
+  });
+}
+
 
