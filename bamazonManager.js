43bec145a3cd713ac6cec10a3602d1fbec720bff
kanikas01 <larvakurt@yahoo.com>
2019-03-07 18:25:50 -0800

    Begin working on addToInventory and addProduct functionality

--- bamazonManager.js
+++ bamazonManager.js
@@ -3,25 +3,11 @@
 
 require('dotenv').config();
 const cTable = require('console.table');
-var customer = require('./db_utils.js');
 var inquirer = require('inquirer');
-
-var connection = customer.connection;
-var showCustomerView = customer.showCustomerView;
-
-var questions = [
-  {
-    type: 'list',
-    name: 'choice',
-    message: 'What would you like to do?',
-    choices: ['View products for sale', 
-              'View low inventory', 
-              'Add to inventory',
-              'Add new product',
-              'Show customer view',
-              'Exit']
-  }
-];
+var db_utils = require('./db_utils.js');
+var connection = db_utils.connection;
+var getProductTableInfo = db_utils.getProductTableInfo;
+var showCustomerView = db_utils.showCustomerView;
 
 // Connect to DB
 connection.connect(function (err) {
@@ -38,6 +24,21 @@ connection.connect(function (err) {
 // ---------- Function definitions ---------- //
 
 function main() {
+
+  var questions = [
+    {
+      type: 'list',
+      name: 'choice',
+      message: 'What would you like to do?',
+      choices: ['View products for sale',
+        'View low inventory',
+        'Add to inventory',
+        'Add new product',
+        'Show customer view',
+        'Exit']
+    }
+  ];
+
   inquirer.prompt(questions).then(answers => {
     switch (answers.choice) {
       case 'View products for sale':
@@ -47,8 +48,10 @@ function main() {
         viewLowInventory(main);
         break;
       case 'Add to inventory':
+        addToInventory(main);
         break;
       case 'Add new product':
+        addNewProduct(main);
         break;
       case 'Show customer view':
         showCustomerView(connection, main);
@@ -99,3 +102,70 @@ function viewLowInventory(callback) {
 }
 
 
+async function addToInventory(callback) {
+  var questions = [
+    {
+      type: 'input',
+      name: 'id',
+      message: "Product ID: ",
+    },
+    {
+      type: 'input',
+      name: 'quantity',
+      message: "Quantity to add: ",
+    }
+  ];
+
+  inquirer.prompt(questions).then(answers => {
+    console.log(answers);
+    connection.query(`SELECT id,
+                      product_name, 
+                      department_name, 
+                      price,
+                      stock_quantity
+                    FROM products`, function (error, results, fields) {
+                      
+        if (error) throw error;
+        if (callback) {
+          callback();
+        }
+      });
+  });
+}
+
+function addNewProduct(callback) {
+  var questions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: "Product name: ",
+    },
+    {
+      type: 'input',
+      name: 'department',
+      message: "Product department: ",
+    },
+    {
+      type: 'input',
+      name: 'price',
+      message: "Product price: ",
+    },
+    {
+      type: 'input',
+      name: 'quantity',
+      message: "Initial quantity: ",
+    }
+  ];
+
+  // display all current products
+  // prompt for product name
+  // prompt for product department
+  // prompt for product price
+  // prompt for product quantity
+
+  // execute UPDATE query
+
+  if (callback) {
+    callback();
+  }
+}
